db2dedf787c5a0e19fa304c0d83876e077be4402
Michael Weißer <michael.weisser@rindenmuehle.de>
2020-11-09 02:56:56 +0100

    Rebase status changes to db_set() instead of my.save()

--- giftcards/giftcards/doctype/gift_card/gift_card.py
+++ giftcards/giftcards/doctype/gift_card/gift_card.py
@@ -15,14 +15,20 @@ class GiftCard(Document):
 
 	def validate(self):
 		self.validate_values()
+		self.validate_customers()
 		self.validate_dates()
-		self.set_status()
 
 	def validate_values(self):
 		if self.target_value and not self.target_value > 0:
 			frappe.throw(_("Target value must be a positive number."))
 		if self.current_value and not self.current_value > 0:
 			frappe.throw(_("Current value must be a positive number."))
+
+	def validate_customers(self):
+		if not self.is_paid and not (self.buying_customer and self.buying_contact and self.buying_address):
+			frappe.throw(_("A buying customer has to be set if the gift card is not paid yet."))
+		if not self.has_received and not (self.receiving_customer and self.receiving_contact and self.receiving_address):
+			frappe.throw(_("A receiving customer has to be set if the gift card has to be sent to the customer."))
 
 	def validate_dates(self):
 		if (self.valid_from and self.valid_to) and self.valid_from > self.valid_to:
@@ -31,13 +37,11 @@ class GiftCard(Document):
 			frappe.throw(_("Valid to date has to be in the future."))
 
 
-	def before_submit(self):
+	def on_submit(self):
 		if self.is_paid == 1:
-			self.current_value = self.target_value
-			self.per_current_value = 100
-		self.posting_date = today()
-		self.set_status()
-		#self.save()
+			self.db_set('current_value', self.target_value)
+		self.db_set('posting_date', today())
+		self.set_status(update=True)
 
 	def on_cancel(self):
 		self.set_status(update=True)
@@ -61,41 +65,51 @@ class GiftCard(Document):
 		elif self.docstatus == 2:
 			self.status = 'Cancelled'
 
+		# Update Current Value Percentage
+		if self.current_value and self.target_value:
+			self.per_current_value = flt(self.current_value / self.target_value * 100, 1)
+		else:
+			self.per_current_value = 0
+
 		if update:
+			old_doc = self.get_doc_before_save()
+			if old_doc.status != self.status:
+				self.add_comment("Label", _(self.status))
 			self.db_set('status', self.status, update_modified = update_modified)
+			self.db_set('per_current_value', self.per_current_value)
+
 
 	@frappe.whitelist()
 	def update_status_dialog(self, is_paid=0, has_received=0):
 		if is_paid:
-			self.is_paid = 1
-			self.current_value = self.target_value
-			self.per_current_value = 100
+			self.db_set('is_paid', 1)
+			self.db_set('current_value', self.target_value)
+
 		if has_received:
-			self.has_received = 1
-		self.set_status()
-		self.save()
+			self.db_set('has_received', 1)
+		self.set_status(update=True)
 
 
 	@frappe.whitelist()
 	def stop_gift_card(self):
-		self.is_stopped = 0 if self.is_stopped else 1
-		self.set_status()
-		self.save()
+		self.db_set('is_stopped', 0 if self.is_stopped else 1)
+		self.set_status(update=True)
 
 	@frappe.whitelist()
 	def redeem(self, redeemed_value=0):
 		if redeemed_value and self.current_value < redeemed_value:
 			frappe.throw(_("Redeemed value cannot be greater than the current value."))
 
-		if self.allow_partial_redemption:
-			self.current_value -= redeemed_value
-			self.per_current_value = flt(self.current_value / self.target_value * 100)
+		if self.allow_partial_redemption and self.current_value > redeemed_value:
+			self.db_set('current_value', self.current_value - redeemed_value)
+			self.add_comment("Edit", _("redeemed <b>{0}</b>, new value is <b>{1}</b>.".format(
+				frappe.format(redeemed_value, dict(fieldtype="Currency", options="currency")),
+				frappe.format(self.current_value, dict(fieldtype="Currency", options="currency"))
+				)))
 		else:
-			self.current_value = 0
-			self.per_current_value = 0
+			self.db_set('current_value', 0)
 
 		if self.current_value == 0 and not self.redemption_date:
-			#self.redemption_date = today()
 			self.db_set('redemption_date', today())
-		self.set_status()
-		self.save()
+		self.set_status(update=True)
+
